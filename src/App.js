0fbe73be029112916ac1fa4aaf7a5e0d39a39838
Josip Potnar <j.potnar@gmail.com>
2024-02-05 17:20:30 +0000

    Add filter and search functionality

--- src/App.js
+++ src/App.js
@@ -7,24 +7,58 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
-
+  // check for current user so we can return liked posts and ones by followed profiles.
+  const currentUser = useCurrentUser();
+  // need profile_id to filter the posts by.
+  // defaults to an empty string if current user details are still being fetched.
+  const profile_id = currentUser?.profile_id || ""
+  
   return (
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
-              <Route exact path="/signin" render={() => <SignInForm />} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-              {/* id is a URL parameter for our post */}
-              <Route exact path="/posts/:id" render={() => <PostPage />} />
-              <Route render={() => <p>Page not found</p>} />
-            </Switch>
-          </Container>
-        </div>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              // message for when search brings up no results.
+              <PostsPage message="No results found. Adjust your search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              // message for when search brings up no results.
+              <PostsPage message="No results found. Adjust your search keyword or follow a user."
+              filter={`owner__followed__owner__profile=${profile_id}&`} 
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              // message for when search brings up no results.
+              <PostsPage message="No results found. Adjust your search keyword or like a post."
+              filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          {/* id is a URL parameter for our post */}
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route render={() => <p>Page not found</p>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
---
+++ src/pages/posts/PostsPage.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import Post from "./Post";
+import Asset from "../../components/Asset";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+
+import NoResults from "../../assets/no-results.png";
+
+// destructuring message and filter props, with later being an empty string as a default.
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  // keeping track if all data has loaded
+  const [hasLoaded, setHasLoaded] = useState(false);
+  // we'll have to re-fetch posts again when user clicks between home, feed and liked pages.
+  // to detect location, we'll use useLocation react router hook.
+  const { pathname } = useLocation();
+  // used for search functionality
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        // the request string will contain filter parameter, which comes from the filter prop we set in routes.
+        // tells API if we want to see all posts or certain ones.
+        // search query passes data entered in the search field
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
+        // setting HasLoaded to true so spinner no longer spins.
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    // we want spinner displayed before we fetch posts.
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+        // we want this called any time filter or url changes.
+        fetchPosts();
+    }, 1000)
+    return () => {
+        clearTimeout(timer)
+    }
+    // adding query to dependency array so the new req is made when user changed their text search
+  }, [filter, pathname, query]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        {/* search icon */}
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        {/* input field */}
+        <Form
+          className={styles.SearchBar}
+          // passing the event so prevent default refresh behavior.
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            // API req is handled by onChange, not onSubmit
+            onChange={(e) => setQuery(e.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
+        </Form>
+        {/* ternary to display spinner or posts */}
+        {hasLoaded ? (
+          // nested ternary to display posts or message.
+          <>
+            {posts.results.length ? (
+              // map over posts and render each one
+              posts.results.map((post) => (
+                // setPosts is needed to like posts.
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              // show no results asset
+              <Container className={appStyles.Content}>
+                {/* no results image, message we passed down */}
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          // spinner
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default PostsPage;
