30846f61d8b326ad7ae8dac1171a5a0a53d45f2d
Josip Potnar <j.potnar@gmail.com>
2024-02-09 09:19:53 +0000

    Update 404 handling with custom component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import NotFound from "./components/NotFound";
 
 function App() {
   // check for current user so we can return liked posts and ones by followed profiles.
@@ -79,7 +80,7 @@ function App() {
             path="/profiles/:id/edit"
             render={() => <ProfileEditForm />}
           />
-          <Route render={() => <p>Page not found</p>} />
+          <Route render={() => <NotFound />} />
         </Switch>
       </Container>
     </div>
---
+++ src/components/NotFound.js
@@ -0,0 +1,18 @@
+import NoResults from ".././assets/no-results.png";
+import styles from ".././styles/NotFound.module.css";
+import React from "react";
+import Asset from "./Asset";
+
+// rafce
+const NotFound = () => {
+  return (
+    <div className={styles.NotFound}>
+      <Asset
+        src={NoResults}
+        message={"Sorry, the page you're looking for doesn't exist"}
+      />
+    </div>
+  );
+};
+
+export default NotFound;
